cc6a8a9f3cb2cf692cd45513249e2946dfbdb6e1
Matt Langford <matthew.langford95@gmail.com>
2024-05-29 18:58:24 -0400

    Plot title

--- analyze.py
+++ analyze.py
@@ -21,10 +21,13 @@ dt = imu_df['time'].diff().median().total_seconds()
 sample_rate = 1.0 / dt
 print(f"Sample rate: {sample_rate:.3f}hz")
 
-fig, axs = plt.subplots(2, 1, figsize=(30, 10), height_ratios=[3, 1])
+
+fig, axs = plt.subplots(2, 1, figsize=(20, 10), height_ratios=[3, 1])
+fig.canvas.manager.set_window_title("specgram")
+fig.suptitle('Specgram of frequency', fontsize=16)
 axs[0].specgram(np.sqrt(imu_df['mag2']), Fs=sample_rate, NFFT=args.nfft, noverlap=32, cmap="plasma", detrend="mean")
 axs[0].set_xlabel("time (s)")
-axs[0].set_ylabel("freq")
+axs[0].set_ylabel("freq (hz)")
 
 bins = np.linspace(0, 0.1, 100)
 axs[1].hist(imu_df['time'].diff().dt.total_seconds(), bins=bins)
