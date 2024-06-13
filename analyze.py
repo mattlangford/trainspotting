32acbb8ed7b6228b8f275dddc592494f9982854d
Matt Langford <matthew.langford95@gmail.com>
2024-06-12 23:55:59 -0400

    more labeling

--- analyze.py
+++ analyze.py
@@ -24,6 +24,9 @@ parser.add_argument("--max-power", default=-40, type=int)
 parser.add_argument("--min-power", default=-80, type=int)
 parser.add_argument("--start", default=None, type=str)
 parser.add_argument("--trim", default=1, type=int)
+
+parser.add_argument("--detect-threshold", default=-65, type=int)
+parser.add_argument("--detect-rel-height", default=0.75, type=int)
 args = parser.parse_args()
 
 df = load_csv(args.filename)
@@ -56,22 +59,35 @@ print(f"Max Power: {10 * np.log10(Pxx.max()):.3f}")
 axs[0].set_ylabel("freq (hz)")
 
 if args.start:
-    start_time = pd.to_datetime(args.start)
-    fmt = lambda x, pos: '{}\n{}'.format(x, start_time + datetime.timedelta(seconds=x))
+    def to_time(x):
+        start_time = pd.to_datetime(args.start)
+        return (start_time + datetime.timedelta(seconds=x)).strftime("%m-%d %H:%M")
+
+    fmt = lambda x, pos: '{}\n{}'.format(x, to_time(x))
     axs[0].xaxis.set_major_formatter(mpl.ticker.FuncFormatter(fmt))
+else:
+    def to_time(x):
+        return x
 
-# num_bins = len(bins)
-# step = max(1, int(args.x_ticks * 60 / sample_rate))
-# axs[0].set_xticks(bins[::step])
-# times = df['time'].iloc[0] + pd.to_timedelta(bins[::step], unit="s")
-# axs[0].set_xticklabels([f"{time.strftime('%m-%d %H:%M:%S')}" for time in times], rotation=45, ha='right')
+power_data = 10 * np.log10(np.sum(Pxx, axis=0) / (Pxx.shape[1]))
+peaks, _ = scipy.signal.find_peaks(power_data, height=args.detect_threshold, distance=10)
+results_half = np.array(scipy.signal.peak_widths(power_data, peaks, rel_height=args.detect_rel_height)[1:])
+ys = results_half[0]
+starts = bins[results_half[1].astype(int)]
+ends = bins[results_half[2].astype(int) + 1]
 
-axs[1].plot(bins, 10 * np.log10(np.sum(Pxx, axis=0) / (Pxx.shape[1])))
+for s, e, y in zip(starts, ends, ys):
+    print(f"Starting at {to_time(s)} for {e - s:.1f}s")
+    axs[1].text(e, y, f"{e - s:.1f}s", color='r')
+    axs[1].text(s, y, f"{to_time(s)}", color='r',  horizontalalignment='right')
+
+axs[1].hlines(ys, starts, ends, color='r')
+axs[1].plot(bins, power_data)
+axs[1].scatter(bins[peaks], power_data[peaks], marker='x', c='r')
 axs[1].set_xlabel("time (s)")
 axs[1].set_ylabel("power (dB)")
 
 fig.tight_layout()
 fig.subplots_adjust(left=0.10, right=0.95, top=0.95, bottom=0.10)
-
 
 plt.show()
